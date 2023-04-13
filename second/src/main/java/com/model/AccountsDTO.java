787bdec9ce2dc4e4aa040e0aea5df06b39012bec
Bhuvan-sai <kurubabhuvansai.eswarappa@cdw.com>
2023-04-13 14:05:44 +0530

    Ex1latest

---
+++ second/src/main/java/com/model/AccountsDTO.java
@@ -0,0 +1,28 @@
+package com.model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="account")
+public class AccountsDTO {
+	
+	@Id
+	private int acid;
+	private int amount;
+	public int getId() {
+		return acid;
+	}
+	public void setId(int acid) {
+		this.acid = acid;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String toString() {
+		return "AccountsDTO [id="+acid+", amount="+amount+"]";
+	}
+	
+}
